c84c7b43493be274b34746339c537910a9f0ae0b
visionmedia <tj@vision-media.ca>
2010-01-13 10:29:33 -0800

    Spec for removing seed

--- spec/kiwi_spec.rb
+++ spec/kiwi_spec.rb
@@ -44,6 +44,10 @@ describe "Kiwi" do
         File.directory?(File.expand_path('~/.kiwi/seeds/libxmljs/0.1.0/src')).should be_true
       end
       
+      it "should remove seed" do
+        File.file?(File.expand_path('~/.kiwi/seeds/libxmljs/0.1.0/libxmljs.seed')).should be_false
+      end
+      
       it "should abort when already installed" do
         kiwi('install libxmljs').should include('libxmljs 0.1.0 is already installed')
       end
