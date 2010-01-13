93ce2391129945e4fe97d0dfac7966ab178338a4
visionmedia <tj@vision-media.ca>
2010-01-13 10:25:38 -0800

    Started install specs

--- spec/kiwi_spec.rb
+++ spec/kiwi_spec.rb
@@ -15,4 +15,34 @@ describe "Kiwi" do
       kiwi('--help').should include('Usage')
     end
   end
+  
+  describe "install" do
+    describe "with no arguments" do
+      it "should abort with seed name required" do
+        kiwi('install').should include('seed name required')
+      end
+    end
+    
+    describe "<name>" do
+      before :each do
+        kiwi('install libxmljs')  
+      end
+      
+      after :each do
+        `rm -fr ~/.kiwi/seeds`
+      end
+      
+      it "should setup ~/.kiwi/seeds" do
+        File.directory?(File.expand_path('~/.kiwi/seeds')).should be_true
+      end
+      
+      it "should setup ~/.kiwi/seeds/<name>/<version>" do
+        File.directory?(File.expand_path('~/.kiwi/seeds/libxmljs/0.1.0')).should be_true
+      end
+      
+      it "should install the current version" do
+        File.directory?(File.expand_path('~/.kiwi/seeds/libxmljs/0.1.0/src')).should be_true
+      end
+    end
+  end
 end
