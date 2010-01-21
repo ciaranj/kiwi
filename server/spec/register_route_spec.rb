e9e6dc0970310404ff2ffba28fe49b700a30a984
visionmedia <tj@vision-media.ca>
2010-01-21 15:08:02 -0800

    Added POST /user specs

--- server/spec/register_route_spec.rb
+++ server/spec/register_route_spec.rb
@@ -1,10 +1,23 @@
 
 describe "POST /user" do
   describe "when given HTTP basic auth" do
-    it "should description" do
-      post '/user'
-      last_response.should be_ok
-      last_response.body.should include('registration successful')
+    describe "when the user does not exist" do
+      it "should create the user" do
+        post '/user', {}, 'HTTP_AUTHORIZATION' => 'Basic ' + ['foo:bar'].pack('m*')
+        last_response.should be_ok
+        last_response.body.should include('registration successful')
+        User.first(:name => 'foo').should be_a(User)
+      end
+    end
+    
+    describe "when the user exists" do
+      it "should respond with 500" do
+        User.create :name => 'tj', :password => 'foobar'
+        post '/user', {}, 'HTTP_AUTHORIZATION' => 'Basic ' + ['tj:bar'].pack('m*')
+        last_response.status.should == 500
+        last_response.body.should include('registration failed')
+        User.first(:name => 'tj').password.should == 'foobar'
+      end
     end
   end
   
