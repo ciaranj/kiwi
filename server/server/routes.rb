37ada2862e6ed751640957aed21d48a57535370b
visionmedia <tj@vision-media.ca>
2010-01-20 16:42:49 -0800

    Super rough auth implementation

--- server/server/routes.rb
+++ server/server/routes.rb
@@ -1,4 +1,55 @@
 
+require 'dm-core'
+require 'dm-validations'
+require 'digest/md5'
+
+DataMapper.setup :default, 'sqlite3::memory:'
+
+# TODO: halt 500 -> #error ?
+
+class User
+  include DataMapper::Resource
+  property :id,       Serial
+  property :name,     String, :index => true, :required => true
+  property :password, String, :index => true, :required => true
+  validates_is_unique :name, :on => :register
+  has n, :seeds
+end
+
+class Seed
+  include DataMapper::Resource
+  property :id,       Serial
+  property :name,     String, :index => true, :required => true
+  validates_is_unique :name, :on => :register
+  belongs_to :user
+end
+
+DataMapper.auto_migrate!
+
+helpers do
+  def credentials
+    auth ||=  Rack::Auth::Basic::Request.new request.env
+    halt 500, 'http basic auth credentials required' unless auth.provided? && auth.basic?
+    auth.credentials
+  end
+end
+
+get '/user' do
+  name, password = credentials
+  user = User.first :name => name, :password => Digest::MD5.hexdigest(password)
+  "user #{name}:#{password} currently has #{user.seeds.length} seeds: " + user.seeds.map{ |seed| seed.name }.join(', ')
+end
+
+post '/user' do
+  name, password = credentials
+  user = User.new :name => name, :password => Digest::MD5.hexdigest(password)
+  if user.save :register
+    'registration successful'
+  else
+    halt 500, 'registration failed'
+  end
+end
+
 ##
 # Search seeds, all are listed unless filtered by:
 #
@@ -38,9 +89,18 @@ end
 # Publish seed _name_. Requires _seed_ archive and _info_ file.
 
 post '/:name/?' do
+  name, password = credentials
+  user = User.first(:name => name, :password => Digest::MD5.hexdigest(password)) || halt(500, 'failed to authenticate, register first')
   name = params[:name]
   seed = params[:seed]
   info = params[:info]
+  if inst = Seed.first(:name => name)
+    unless inst.user == user
+      halt 500, "unauthorized to publish #{name}"
+    end
+  else
+    user.seeds.create :name => name
+  end
   halt 500, '<version>.seed required' unless seed
   halt 500, 'seed.yml required' unless info
   version = File.basename seed[:filename], '.seed'
