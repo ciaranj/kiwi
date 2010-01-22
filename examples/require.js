20ec2d6d78356bf5ff5962b9d5d76acefbea2ea5
visionmedia <tj@vision-media.ca>
2010-01-22 07:19:06 -0800

    Added runtime require support for "n.n.n" instead of "= n.n.n"
    both are valid

--- examples/require.js
+++ examples/require.js
@@ -3,5 +3,6 @@ require.paths.unshift('lib')
 var kiwi = require('kiwi'),
     sys = require('sys')
 
-kiwi.require('oo', '= 1.2.0')
-sys.p(Class.version)
+require('sys').p(kiwi.require('oo', '= 1.1.0'))
+require('sys').p(kiwi.require('oo', '1.1.0'))
+require('sys').p(kiwi.require('oo', '1.2.0'))
