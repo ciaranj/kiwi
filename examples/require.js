26ae968d12ad93c9cf1dba41eeec8b6919d2748f
visionmedia <tj@vision-media.ca>
2010-03-04 10:13:11 -0800

    Updated js to support latest node. Closes #102

--- examples/require.js
+++ examples/require.js
@@ -3,6 +3,5 @@ require.paths.unshift('lib')
 var kiwi = require('kiwi'),
     sys = require('sys')
 
-require('sys').p(kiwi.require('oo', '= 1.1.0'))
-require('sys').p(kiwi.require('oo', '1.1.0'))
+require('sys').p(kiwi.require('haml', '= 0.1.2'))
 require('sys').p(kiwi.require('oo', '1.2.0'))
