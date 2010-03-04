3f6c44e54cf98db6aa1ae475dda012fad73319ea
visionmedia <tj@vision-media.ca>
2010-03-04 12:43:06 -0800

    Refactoring specs

--- server/spec/search_route_spec.rb
+++ server/spec/search_route_spec.rb
@@ -1,10 +1,20 @@
 
 describe "GET /search" do
+  before :each do
+    DataMapper.auto_migrate!
+    @user = User.create :name => 'foo', :password => 'bar'
+    @sass = @user.seeds.create :name => 'sass'
+    @sass.versions.create :version => '0.0.1', :description => 'Sass to css engine'
+    @oo = @user.seeds.create :name => 'oo'
+    @oo.versions.create :version => '1.2.0', :description => 'Class implementation for JavaScript'  
+    @oo.versions.create :version => '1.1.0', :description => 'Class implementation'  
+  end
+  
   it "should respond with a formatted list of available seeds / versions" do
     get '/search'
     last_response.should be_ok
     last_response.body.should include("sass : 0.0.1")
-    last_response.body.should include("  oo : 1.2.0")
+    last_response.body.should include("  oo : 1.2.0 1.1.0")
   end
   
   describe "given :name" do
@@ -12,7 +22,7 @@ describe "GET /search" do
       get '/search?name=ass'
       last_response.should be_ok
       last_response.body.should include("sass : 0.0.1")
-      last_response.body.should_not include("  oo : 1.2.0")
+      last_response.body.should_not include("  oo")
     end
   end
 end
