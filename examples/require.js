d79beaa57c7661e9f209211cd7f722de0824c1eb
visionmedia <tj@vision-media.ca>
2010-03-04 15:51:20 -0800

    Better examples

--- examples/require.js
+++ examples/require.js
@@ -2,6 +2,8 @@
 var kiwi = require('kiwi'),
     sys = require('sys')
 
-sys.p(kiwi.require('haml', '= 0.1.2'))
 sys.p(kiwi.require('oo', '1.2.0'))
+sys.p(kiwi.require('oo', '= 1.2.0'))
+sys.p(kiwi.require('oo', '> 1.1.0'))
+sys.p(kiwi.require('oo', '>~ 1.0.0'))
 sys.p(kiwi.require('oo'))
